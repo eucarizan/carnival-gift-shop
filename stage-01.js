e3205fca4343751fcb42aac9951187f79d17d50d
eucarizan <eucarizan@gmail.com>
2024-12-22 03:57:08 +0800

    stage 1 done

--- stage-01.js
+++ stage-01.js
@@ -1,1 +1,15 @@
-// write your code here
+const greeting = "WELCOME TO THE CARNIVAL GIFT SHOP!\n" +
+  "Hello friend! Thank you for visiting the carnival!";
+
+const gifts = ["Teddy Bear", "Big Red Ball", "Huge Bear", "Candy", "Stuffed Tiger", 
+  "Stuffed Dragon", "Skateboard", "Toy Car", "Basketball", "Scary Mask"];
+
+function listGifts() {
+  console.log("Here's the list of gifts:\n");
+  gifts.forEach(e => console.log(e));
+}
+
+(function main() {
+  console.log(greeting);
+  listGifts();
+})();
