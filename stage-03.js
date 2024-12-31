6ed8bb9f99681cc8514aa630f84a30c2ed064265
eucarizan <eucarizan@gmail.com>
2024-12-31 08:31:25 +0800

    stage 3 done

--- stage-03.js
+++ stage-03.js
@@ -1,18 +1,21 @@
+const input = require('sync-input');
 const greeting = `WELCOME TO THE CARNIVAL GIFT SHOP!
 Hello friend! Thank you for visiting the carnival!`;
 
-const gifts = {
- "Teddy Bear": 10,
- "Big Red Ball": 5,
- "Huge Bear": 50,
- "Candy": 8,
- "Stuffed Tiger": 15,
- "Stuffed Dragon": 30,
- "Skateboard": 100,
- "Toy Car": 25,
- "Basketball": 20,
- "Scary Mask": 75
-};
+const gifts = [
+  { id: 1, name: "Teddy Bear", cost: 10 },
+  { id: 2, name: "Big Red Ball", cost: 5 },
+  { id: 3, name: "Huge Bear", cost: 50 },
+  { id: 4, name: "Candy", cost: 8 },
+  { id: 5, name: "Stuffed Tiger", cost: 15 },
+  { id: 6, name: "Stuffed Dragon", cost: 30 },
+  { id: 7, name: "Skateboard", cost: 100 },
+  { id: 8, name: "Toy Car", cost: 25 },
+  { id: 9, name: "Basketball", cost: 20 },
+  { id: 10, name: "Scary Mask", cost: 75 }
+];
+
+let tickets = 0;
 
 function displayGreeting() {
   console.log(greeting);
@@ -26,13 +29,62 @@ function listGifts() {
     console.log(`${i}- ${toy}, Cost: ${gifts[toy]} tickets`);
     i++;
   }
-  */
   Object.entries(gifts).forEach(([gift, cost], index) => {
     console.log(`${index + 1}- ${gift}, Cost: ${cost} tickets`);
   });
+  */
+  gifts.forEach((gift) => {
+    console.log(`${gift.id}- ${gift.name}, Cost: ${gift.cost} tickets`);
+  });
+}
+
+function showMenu() {
+  let choice = Number(input("What do you want to do?\n1-Buy a gift 2-Add tickets 3-Check tickets 4-Show gifts\n"));
+  switch (choice) {
+    case 1:
+      buyGift();
+      break;
+    case 2:
+      addTickets();
+      break;
+    case 3:
+      checkTickets();
+      break;
+    case 4:
+      listGifts();
+      break;
+  }
+}
+
+function buyGift() {
+  let id = Number(input("Enter the number of gift you want to get:\n"));
+  let gift = getGiftById(id);
+  if (gift) {
+    const { name, cost } = gift;
+    console.log(`Here you go, one ${name}!`);
+    tickets -= cost;
+  }
+  checkTickets();
+}
+
+function getGiftById(id) {
+  return gifts.find(gift => gift.id == id) || null;
+}
+
+function checkTickets() {
+  console.log(`Total tickets: ${tickets}`);
+}
+
+function addTickets() {
+  let ticketToAdd = Number(input("Enter the ticket amount: "));
+  tickets += ticketToAdd;
+  checkTickets();
 }
 
 (function main() {
+  tickets = 100;
   displayGreeting();
   listGifts();
+  showMenu();
+  console.log("Have a nice day!");
 })();
